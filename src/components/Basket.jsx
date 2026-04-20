fb8a72a3373112133e2cd0ff74acd8ecaca9e6be
KatKhir <kasa0004@stud.kea.dk>
2026-04-20 09:11:40 +0200

    feat: add Basket, FavoriteIcon, Header, and ProductList components

---
+++ src/components/Basket.jsx
@@ -0,0 +1,71 @@
+import { FaShoppingCart } from "react-icons/fa";
+import { IoTrashBinOutline } from "react-icons/io5";
+
+const Basket = () => {
+  return (
+    <div className=" w-[30vw] bg-[#FAF6EA] h-fit rounded-2xl mr-10 mb-5 mt-5 pl-4 pb-4 ">
+      <div className="flex justify-center">
+        <FaShoppingCart className="w-8 h-8 mt-4 mb-6" />
+      </div>
+      <ul className="justify-center items-start">
+        <li className="flex gap-3 mb-3">
+          <img src="/img/farver.webp" alt="Billede af faver" width={70} height={80} />
+          <div className="flex flex-col justify-between w-full">
+            <div>
+              <h3 className="text-sm text-gray-400 underline decoration-gray-400 underline-offset-2.5">MÆRKE</h3>
+              <h4 className="text-sm">NAVN PÅ PRODUKT</h4>
+            </div>
+            <div className="flex justify-between items-end">
+              <p className="font-bold text-lg">00,00 kr.</p>
+              <IoTrashBinOutline className="cursor-pointer w-5 h-auto mr-4" />
+            </div>
+          </div>
+        </li>
+        <hr className="border-0.5 border-gray-300 mb-3 mr-4" />
+        <li className="flex gap-3 mb-3">
+          <img src="/img/farver.webp" alt="Billede af faver" width={70} height={80} />
+          <div className="flex flex-col justify-between w-full">
+            <div>
+              <h3 className="text-sm text-gray-400 underline decoration-gray-400 underline-offset-2.5">MÆRKE</h3>
+              <h4 className="text-sm">NAVN PÅ PRODUKT</h4>
+            </div>
+            <div className="flex justify-between items-end">
+              <p className="font-bold text-lg">00,00 kr.</p>
+              <IoTrashBinOutline className="cursor-pointer w-5 h-auto mr-4" />
+            </div>
+          </div>
+        </li>
+        <hr className="border-0.5 border-gray-300 mb-3 mr-4" />
+        <li className="flex gap-3 mb-3">
+          <img src="/img/farver.webp" alt="Billede af faver" width={70} height={80} />
+          <div className="flex flex-col justify-between w-full">
+            <div>
+              <h3 className="text-sm text-gray-400 underline decoration-gray-400 underline-offset-2.5">MÆRKE</h3>
+              <h4 className="text-sm">NAVN PÅ PRODUKT</h4>
+            </div>
+            <div className="flex justify-between items-end">
+              <p className="font-bold text-lg">00,00 kr.</p>
+              <IoTrashBinOutline className="cursor-pointer w-5 h-auto mr-4" />
+            </div>
+          </div>
+        </li>
+        <hr className="border-0.5 border-gray-300 mb-3 mr-4" />
+        <li className="flex gap-3 mb-3">
+          <img src="/img/farver.webp" alt="Billede af faver" width={70} height={80} />
+          <div className="flex flex-col justify-between w-full">
+            <div>
+              <h3 className="text-sm text-gray-400 underline decoration-gray-400 underline-offset-2.5">MÆRKE</h3>
+              <h4 className="text-sm">NAVN PÅ PRODUKT</h4>
+            </div>
+            <div className="flex justify-between items-end">
+              <p className="font-bold text-lg">00,00 kr.</p>
+              <IoTrashBinOutline className="cursor-pointer w-5 h-auto mr-4" />
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Basket;
---
+++ src/components/FavoriteElement.jsx
@@ -0,0 +1,12 @@
+import { FaRegHeart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
+
+const FavoriteIcon = () => {
+  return (
+    <div>
+      <FaRegHeart className="absolute top-4 right-4 w-8 h-8" />
+    </div>
+  );
+};
+
+export default FavoriteIcon;
