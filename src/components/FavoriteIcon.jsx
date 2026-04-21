a7f5def20259c0228830718fd1ded5ff21bb6c04
alma05858 <alca0004@stud.kea.dk>
2026-04-21 12:07:36 +0200

    feat: implement FavoriteIcon component for managing favorites

--- src/components/FavoriteIcon.jsx
+++ src/components/FavoriteIcon.jsx
@@ -1,12 +1,30 @@
+"use client";
 import { FaRegHeart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
+import useFavorites from "@/store/favorite";
 
-const FavoriteIcon = () => {
-  return (
-    <div>
-      <FaRegHeart className="w-8 h-8" />
-    </div>
-  );
+const FavoriteIcon = ({ id, product }) => {
+  const { favorites, setFavorite, removeFavorite } = useFavorites();
+
+  if (favorites.some((favorite) => favorite.id === id)) {
+    return (
+      <FaHeart
+        className="w-8 h-8 cursor-pointer"
+        onClick={() => {
+          removeFavorite(id);
+        }}
+      />
+    );
+  } else {
+    return (
+      <FaRegHeart
+        className="w-8 h-8 cursor-pointer"
+        onClick={() => {
+          setFavorite(id, product);
+        }}
+      />
+    );
+  }
 };
 
 export default FavoriteIcon;
