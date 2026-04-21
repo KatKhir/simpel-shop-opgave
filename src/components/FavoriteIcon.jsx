31586c71666877cad27626d166667d1466c7e6f8
alma05858 <alca0004@stud.kea.dk>
2026-04-21 11:35:57 +0200

    refactor: update FavoriteIcon positioning and remove unused components

--- src/components/FavoriteIcon.jsx
+++ src/components/FavoriteIcon.jsx
@@ -4,7 +4,7 @@ import { FaHeart } from "react-icons/fa";
 const FavoriteIcon = () => {
   return (
     <div>
-      <FaRegHeart className="absolute top-4 right-4 w-8 h-8" />
+      <FaRegHeart className="w-8 h-8" />
     </div>
   );
 };
