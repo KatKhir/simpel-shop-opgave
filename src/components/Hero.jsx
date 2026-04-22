601d0f8e0c13f2d11ed1c558e72b7088ed21bf7c
hube0001 <hube0001@stud.kea.dk>
2026-04-22 14:27:05 +0200

    Font lavet

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,13 +12,13 @@ const Hero = () => {
           <Image src="/hero_billede.png" fill alt="Hero" className="object-cover" />
 
           <div className="absolute text-8xl flex-row p-18 m-0 gap-30 text-white">
-            <p>Skab din stil.</p>
-            <p className="ml-40 mt-5"> Lev din vibe.</p>
+            <p>Create your style.</p>
+            <p className="ml-40 mt-5"> Live your vibe.</p>
           </div>
 
           <div className="flex justify-center p-[150px] mt-[250px] ml-[500px]">
             <Link href="/productListPage" className="absolute font-sans bg-white text-black px-6 py-3 rounded-lg font-semibold hover:bg-gray-200 transition">
-              Se vores udvalgte varer
+              See our selected products
             </Link>
           </div>
         </div>
