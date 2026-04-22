d2add659763b139a989a722e08bb05e9000ea001
alma05858 <alca0004@stud.kea.dk>
2026-04-22 13:52:02 +0200

    feat: implement ProductSingle and Related components for product detail page

---
+++ src/components/ProductSingle.jsx
@@ -0,0 +1,31 @@
+"use client";
+
+import FavoriteIcon from "@/components/FavoriteIcon";
+import Link from "next/link";
+import useBasket from "@/store/Basket";
+
+const ProductSingle = ({ product }) => {
+  const addToCart = useBasket((state) => state.addToCart);
+  return (
+    <div className="flex flex-col md:flex-row mb-10">
+      <img className="rounded-lg bg-white mr-5" src={product.thumbnail} alt={product.title} width={350} height={350} />
+      <div>
+        <Link className="block my-2" href="/productListPage">
+          Tilbage til produkter
+        </Link>
+        <h3 className="mb-1 text-xl text-gray-400 underline decoration-gray-400 underline-offset-2.5">{product.brand}</h3>
+        <h4 className="text-black pb-4">{product.title}</h4>
+        <p className="mb-4 max-w-[50ch] text-black pb-4">{product.description}</p>
+        <p className="mb-3 text-black font-bold text-xl">{product.price} kr.</p>
+        <div className="flex gap-4">
+          <button onClick={() => addToCart(product)} className="cursor-pointer border border-black px-2 rounded-md">
+            Add +
+          </button>
+          <FavoriteIcon id={product.id} product={product} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductSingle;
