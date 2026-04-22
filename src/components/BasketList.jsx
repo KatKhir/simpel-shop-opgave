3e84706167c95d8766f638ab08d9ce16af605b3d
KatKhir <kasa0004@stud.kea.dk>
2026-04-22 13:50:18 +0200

    feat: add quantity management to basket items and update total price display

--- src/components/BasketList.jsx
+++ src/components/BasketList.jsx
@@ -9,6 +9,7 @@ import Link from "next/link";
 const Basket = () => {
   const cart = useBasket((state) => state.cart);
   const removeFromCart = useBasket((state) => state.removeFromCart);
+  const decreaseQuantity = useBasket((state) => state.decreaseQuantity);
 
   return (
     <div className="w-75 bg-[#FAF6EA] h-fit rounded-2xl mr-10 mb-5 mt-5 pl-4 pb-4">
@@ -34,16 +35,16 @@ const Basket = () => {
                   </div>
 
                   <div className="flex justify-between items-end">
-                    <p className="font-bold text-lg">{item.price} kr.</p>
-
-                    <IoTrashBinOutline onClick={() => removeFromCart(item.id)} className="cursor-pointer mr-4 text-red-800" width={10} height={10} />
+                    <p className="font-bold text-lg"> {(item.price * item.quantity).toFixed(2)} kr.</p>
+                    <IoTrashBinOutline onClick={() => (item.quantity === 1 ? removeFromCart(item.id) : decreaseQuantity(item.id))} className="cursor-pointer mr-4 text-red-800" width={10} height={10} />
                   </div>
+                  {item.quantity > 1 && <span className=" text-gray-500 text-[0.9rem] my-2 font-bold w-5 h-5 ">Antal: {item.quantity}</span>}
                 </div>
               </li>
             ))}
           </ul>
           <div className="flex justify-center pb-3">
-            <Link href="/kurv">
+            <Link href="/Betaling">
               <button className="text-xs font-semibold border border-black py-2 px-4 rounded-xl cursor-pointer transition-all duration-150 hover:scale-105 active:scale-100 ">GÅ TIL BETALING</button>
             </Link>
           </div>
