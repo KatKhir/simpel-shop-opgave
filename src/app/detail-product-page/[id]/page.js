f311a13212da8c984129aac1c68d7209002d0ff3
alma05858 <alca0004@stud.kea.dk>
2026-04-22 14:23:47 +0200

    feat: implement DetailPageClient component for product detail view and related products

--- src/app/detail-product-page/[id]/page.js
+++ src/app/detail-product-page/[id]/page.js
@@ -1,6 +1,4 @@
-import BasketList from "@/components/BasketList";
-import Related from "@/components/Related";
-import ProductSingle from "@/components/ProductSingle";
+import DetailPageClient from "@/components/DetailPageClient";
 
 async function getProduct(id) {
   const res = await fetch(`https://dummyjson.com/products/${id}`);
@@ -18,15 +16,5 @@ export default async function DetailProductPage({ params }) {
   const product = await getProduct(id);
   const relatedProducts = await getRelatedProducts(product.category, Number(id));
 
-  return (
-    <main>
-      <div className="flex">
-        <div className="w-[70vw] bg-[#FAF6EA] rounded-2xl p-4 mr-5 mt-5 mb-5 ml-10">
-          <ProductSingle product={product} />
-          <Related relatedProducts={relatedProducts} />
-        </div>
-        <BasketList />
-      </div>
-    </main>
-  );
+  return <DetailPageClient product={product} relatedProducts={relatedProducts} />;
 }
